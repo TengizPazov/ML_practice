e4f1d10ebcc3b1f7129eef9830ab3ec61f1855ff
Tengiz Pazov <pazov.tg@phystech.edu>
2025-10-16 16:04:07 +0300

    added roc curve

--- logreg/roc_auc/ex_roc_auc.py
+++ logreg/roc_auc/ex_roc_auc.py
@@ -5,6 +5,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
+def calculate_auc(x, y):
+    """Вычисление AUC методом трапеций"""
+    auc = 0.0
+    for i in range(1, len(x)):
+        width = x[i] - x[i-1]
+        height = (y[i] + y[i-1]) / 2
+        auc += width * height
+    return auc
 #загрузка данных
 url = "https://raw.githubusercontent.com/Statology/Python-Guides/main/default.csv"
 data = pd.read_csv(url)
@@ -24,5 +32,14 @@ y = []
 for value in result:
     x.append(value[0])
     y.append(value[1])
-plt.plot(x, y)
-plt.show()
+AUC = calculate_auc(x, y)
+print(AUC)
+plt.plot(x, y, label=f'ROC curve (AUC = {round(AUC, 3)})')
+x0 = [0.0, 1.0]
+y0 = [0.0, 1.0]
+plt.plot(x0, y0, linestyle='--', label='random model')
+plt.legend()
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plt.title('ROC-AUC curve')
+plt.savefig('ROC-AUC.png')
